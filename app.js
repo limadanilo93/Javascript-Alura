7b500e0ce7187e7b2f2508737360687d91e384d0
limadanilo93 <88122215+limadanilo93@users.noreply.github.com>
2022-02-25 23:04:11 -0300

    test: another test

--- app.js
+++ app.js
@@ -23,12 +23,32 @@ const alunos ={
     nota2: [6, 7, 8],
      
 }
-console.log(alunos)
+console.log(alunos.nota1)
+function teste(obj, key){
+    const t = {
+        name: ['Laís', 'João', 'Felícia'],
+        nota1: [10, 8, 9],
+        nota2: [6, 7, 8],
+         
+    }
+    const k = key
+    const valor = t[k];
+    console.log(valor)
+}
+teste(alunos, 'nota1')
+// console.log(alunos)
 
 
-alunos.media = alunos.nota1.map((n,i) => (n + alunos.nota2[i])/2)
-console.log(alunos)
+// alunos.media = alunos.nota1.map((n,i) => (n + alunos.nota2[i])/2)
+// console.log(alunos)
 
-for(i=0;i<alunos.name.length;i++){
-    console.log(`O(a) aluno(a) ${alunos.name[i]} ficou com média ${alunos.media[i]}`)
-}    
+// for(i=0;i<alunos.name.length;i++){
+//     console.log(`O(a) aluno(a) ${alunos.name[i]} ficou com média ${alunos.media[i]}`)
+// }
+str = '1234'
+
+function myFunction(str){
+const last = str.substring(0, str.length/2)
+return last
+}
+console.log(myFunction(str))
