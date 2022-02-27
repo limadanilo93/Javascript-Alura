661d62ffe189be0a628863592638ce15ee0c9fa4
limadanilo93 <88122215+limadanilo93@users.noreply.github.com>
2022-02-27 00:22:45 -0300

    feat: objeto cliente

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const alunos ={
     nota2: [6, 7, 8],
      
 }
-console.log(alunos.nota1)
+//console.log(alunos.nota1)
 function teste(obj, key){
     const t = {
         name: ['Laís', 'João', 'Felícia'],
@@ -35,7 +35,7 @@ function teste(obj, key){
     const valor = t[k];
     console.log(valor)
 }
-teste(alunos, 'nota1')
+//teste(alunos, 'nota1')
 // console.log(alunos)
 
 
@@ -51,4 +51,53 @@ function myFunction(str){
 const last = str.substring(0, str.length/2)
 return last
 }
-console.log(myFunction(str))
+//console.log(myFunction(str))
+const ir = (renda)=> {return renda<1903 ? 0 : renda<2826 ? 0.075 : renda<3751 ? 0.15 : renda<4664 ? 0.225 : 0.275}
+const cliente = {
+    nome: 'Danilo',
+    idade: 28,
+    cpf: '3545424574212',
+    email: 'kala@abhs.com',
+    salario: 4300,
+    liquido: (sal, aliq) => { return sal - sal*aliq},
+    saldo: 641,
+
+}
+
+
+//console.log(cliente)
+
+
+
+
+//console.log(cliente)
+
+
+//console.log(cliente)
+const despesasFixas = document.getElementById('despesas').value
+let despesasVar = cliente.salario*0.3 + cliente.saldo*0.05
+function alteraSaldo(){
+  
+    cliente.saldo = cliente.saldo + cliente.liquido(cliente.salario, ir(cliente.salario)) - despesasFixas - despesasVar
+    return cliente.saldo
+}
+function alteraSalario(aumento){
+
+    cliente.salario = aumento*cliente.salario
+}
+
+let saldoJan = alteraSaldo()
+let saldoFev = alteraSaldo()
+let saldoMar = alteraSaldo()
+let saldoAbr = alteraSaldo()
+let saldoJun = alteraSaldo()
+let saldoJul = alteraSaldo()
+alteraSalario(1.2)
+let saldoAgo = alteraSaldo()
+let saldoSet = alteraSaldo()
+let saldoOut = alteraSaldo()
+let saldoNov = alteraSaldo()
+let saldoDez = alteraSaldo()
+console.log(cliente)
+console.log(`O saldo em Fevereiro foi de R$ ${saldoFev.toFixed(2)}`)
+console.log(`O saldo em Dezembro foi de R$ ${saldoDez.toFixed(2)}`)
